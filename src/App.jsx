44e954ad3b9a4b4a84e12f9fff994527b475d8ed
ajoy267 <andrew.joy12@hotmail.com>
2022-02-08 14:53:32 -0800

    refactoring a little bit with the files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from './views/Home';
+import Home from './views/Home/Home';
 
 function App() {
   return (
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import GuestForm from '../components/GuestForm/GuestForm';
-import Header from '../components/Header/Header';
-import { EntryProvider } from '../context/EntryContext';
+import GuestForm from '../../components/GuestForm/GuestForm';
+import Header from '../../components/Header/Header';
+import { EntryProvider } from '../../context/EntryContext';
 
 export default function Home() {
   return (
