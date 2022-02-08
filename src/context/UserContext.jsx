8f0c19949b9b1d0e42934b52976a8bd1c9dee6de
ajoy267 <andrew.joy12@hotmail.com>
2022-02-08 14:32:54 -0800

    worte my entry context and wrapped my app.jsx and my home.jsx in providers, now im working on my guest form

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from 'react';
+
+const UserContext = createContext();
+
+function UserProvider({ children }) {
+  const [user, setUser] = useState({});
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+}
+
+function useUser() {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new Error('useUser needs to be inside UserProvider');
+  }
+  return context;
+}
+
+export { UserProvider, useUser };
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import Header from '../components/Header';
+import GuestForm from '../components/GuestForm/GuestForm';
+import Header from '../components/Header/Header';
+import { EntryProvider } from '../context/EntryContext';
 
 export default function Home() {
   return (
     <div>
-      <Header />
+      <EntryProvider>
+        <Header />
+        <GuestForm />
+      </EntryProvider>
       Home
     </div>
   );
