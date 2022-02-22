557ba232296e5396e0e422c858fe15f6a8b5d07b
ajoy267 <andrew.joy12@hotmail.com>
2022-02-22 10:50:24 -0800

    added login form, (signing in still doesnt work but i think thats because i need to continue refactoring)

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -1,5 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
+import { useForm } from '../../hooks/useForm';
 
 export default function LoginForm() {
-  return <div>LoginForm</div>;
+  const [error, setError] = useState('');
+  const user = useUser();
+  const history = useHistory();
+  const location = useLocation();
+  const { formState, handleFormChange } = useForm({ username: '', password: '' });
+  const { from } = location.state || { from: { pathname: '/' } };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const loginWasSuccessful = user.login(formState.username, formState.password);
+
+    {
+      loginWasSuccessful
+        ? history.replace(from.pathname)
+        : setError('Login was unsuccessful, Please try again');
+    }
+  };
+  return (
+    <main>
+      <h4>Sign In</h4>
+      <form onSubmit={handleLogin}>
+        <label>
+          Username:
+          <input id="username" name="username" type="username" onChange={handleFormChange} />
+        </label>
+        <label>
+          Password
+          <input id="password" name="password" type="password" onChange={handleFormChange} />
+        </label>
+        <button type="submit" aria-label="sign in">
+          Sign In
+        </button>
+      </form>
+      {error && <h5>{error}</h5>}
+    </main>
+  );
 }
