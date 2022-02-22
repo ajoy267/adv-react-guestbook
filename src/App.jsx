3cad60a1c62b962bee0ca2f7f7c884f03d971972
ajoy267 <andrew.joy12@hotmail.com>
2022-02-21 16:57:10 -0800

    made login view file and made my router, switch, and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css';
+import { EntryProvider } from './context/EntryContext';
+import Login from './views/Auth/Login';
 import Home from './views/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Home />
+      <EntryProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
+      </EntryProvider>
     </div>
   );
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 import EntryList from '../../components/EntryList/EntryList';
 import GuestForm from '../../components/GuestForm/GuestForm';
 import Header from '../../components/Header/Header';
-import { EntryProvider } from '../../context/EntryContext';
 import Animation from '../../hooks/Animation';
 import DarkModeToggle from '../../hooks/DarkMode';
 
 export default function Home() {
   return (
     <div>
-      <EntryProvider>
-        <Header />
-        <DarkModeToggle />
-        <GuestForm />
-        <Animation />
-        <EntryList />
-      </EntryProvider>
+      <Header />
+      <DarkModeToggle />
+      <GuestForm />
+      {/* <Animation /> */}
+      <EntryList />
     </div>
   );
 }
